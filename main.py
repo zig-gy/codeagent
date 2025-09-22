d86bbdef9b07bdf39f3db238253dc6a894259bd2
zig-gy <benjamin.f.chadwick@proton.me>
2025-09-22 14:46:56 -0300

    added prompt passed as argument, verbose flag, and scaffolding for conversation management

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
+    if len(sys.argv) < 2:
+        print("No prompt provided")
+        sys.exit(1)
+
+    prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
+    flag = ""
+    if len(sys.argv) > 2:
+        flag = sys.argv[2]
+
+    verbose = False
+    if flag == "--verbose":
+        verbose = True
+
     client = genai.Client(api_key = api_key)
     response = client.models.generate_content(
             model='gemini-2.0-flash-001',
-            contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+            contents= messages
             )
+    
+    if verbose: print("User prompt:", prompt)
     print("Response: " + response.text)
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if verbose: print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    if verbose: print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 if __name__ == "__main__":
     main()
